089c15c7ffa2834cec36405d8521a92f13999a1f
xing zhang <zhangxing@broada.com>
2020-10-28 18:00:10 +0800

    commit

--- src/main/scala/nick/leetcode/q100/Q1.scala
+++ src/main/scala/nick/leetcode/q100/Q1.scala
@@ -2,7 +2,7 @@ package nick.leetcode.q100
 
 /**
   * 1. 两数之和
-  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
   * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
   * 示例:
   * 给定 nums = [2, 7, 11, 15], target = 9
@@ -17,13 +17,13 @@ object Q1 {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     var recordMap: Map[Int, Int] = Map()
     for (i <- 0 until (nums.length)) {
-      val result: Int = target - nums(i);
+      val result: Int = target - nums(i)
       if (recordMap.contains(result)) {
-        return Array(recordMap(result), i);
+        return Array(recordMap(result), i)
       }
       recordMap += (nums(i) -> i)
     }
-    return null
+    return Array()
   }
 
   def main(args: Array[String]): Unit = {
